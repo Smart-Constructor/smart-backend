97d3c0757229fe2ff6024d078547b7e624009f78
jansenosorio <dev.osorio93@gmail.com>
2023-06-24 17:35:15 -0300

    ✨chore: update dockercompose file

--- dist/main.js
+++ dist/main.js
@@ -3,13 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
+const config_1 = require("@nestjs/config");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
     app.useGlobalPipes(new common_1.ValidationPipe({
         whitelist: true,
     }));
-    await app.listen(3000);
+    const configService = app.get(config_1.ConfigService);
+    await app.listen(configService.get('PORT'));
+    console.log(`Server is running at PORT:${configService.get('PORT')}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
